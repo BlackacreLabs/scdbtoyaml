789155e804934117f3a6f2dbc56b12358609142c
Kyle Mitchell <kyleevanmitchell@gmail.com>
2012-11-05 19:48:07 -0600

    most important code decoding

--- lib/SCDBtoYAML.rb
+++ lib/SCDBtoYAML.rb
@@ -1,6 +1,7 @@
 require "SCDBtoYAML/version"
 require "SCDBtoYAML/cli"
 require "SCDBtoYAML/converter"
+require "SCDBtoYAML/coding"
 
 module SCDBtoYAML
 end
--- lib/SCDBtoYAML/converter.rb
+++ lib/SCDBtoYAML/converter.rb
@@ -20,8 +20,81 @@ module SCDBtoYAML
     end
 
     # TODO: apply the codebook
-    def self.post_process(hashes)
-      hashes
+    TRANSFORMS = {
+      'id' => lambda { |x| x['caseId'] },
+      'decided' => 'dateDecision',
+      'argued' => 'dateArgument',
+      'reargued' => 'dateReargument',
+      'style' => 'caseName',
+      'chief justice' => 'chief',
+      'voting' => lambda {|x|
+        {
+          'majority' => x['majVotes'],
+          'minority' => x['minVotes'],
+          'split' => x['splitVote'] == 1,
+          'unclear' => x['voteUnclear'] == 1,
+        }
+      },
+      'relief' => lambda {|x|
+        case x['partyWinning']
+        when 0
+          'none'
+        when 1
+          'granted'
+        when 2
+          'unclear'
+        end
+      },
+      'law' => lambda {|x|
+        {
+          'area' => Coding::LEGAL_AREA[x['lawType']],
+          'provision' => Coding::LEGAL_PROVISIONS[x['lawSupp']]
+        }
+      },
+      'effect' => lambda {|x|
+        { 'precedent altered' => x['precedentAlteration'] == 1 }
+      },
+      'parties' => lambda {|x|
+        {
+          'petitioner' => {
+            'description' => Coding::PARTIES[x['petitioner']],
+            'state' => Coding::STATES[x['petitionerState']]
+          },
+          'repsondent' => {
+            'description' => Coding::PARTIES[x['respondent']],
+            'state' => Coding::STATES[x['respondentState']]
+          }
+        }
+      },
+      'term' => lambda {|x| x['docketId'][0,4].to_i},
+      'docket number' => 'docket',
+      'posture' => lambda {|x|
+        {'administrative' => Coding::ADMIN_ACTION[x['adminAction']]}
+      },
+      'majority opinion' => lambda {|x|
+        {
+          'writer' => Coding::JUSTICES[x['majOpinWriter']].last,
+          'assigner' => Coding::JUSTICES[x['majOpinAssigner']].last
+        }
+      }
+    }
+    def self.post_process(hash)
+      out = { }
+      TRANSFORMS.each do |key, action|
+        case action
+        when Proc
+          out[key] = action.call(hash)
+        when String
+          out[key] = hash[action]
+        end
+      end
+      out['citataions'] = {
+        'United State Reports' => hash['usCite'],
+        'Supreme Court Reporter' => hash['sctCite'],
+        'Lawyers Edition' => hash['ledCite'],
+        'Lexis Nexis' => hash['lexisCite']
+      }
+      out
     end
   end
 end
